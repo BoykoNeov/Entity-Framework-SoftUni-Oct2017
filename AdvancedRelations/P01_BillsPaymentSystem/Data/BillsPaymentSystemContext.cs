0f3a6689128f2187c0d43ce5313b825ae6d41153
BoykoNeov <boikoneov@gmail.com>
2017-11-16 22:08:47 +0200

    :watermelon: work on 'advanced relations'

---
+++ AdvancedRelations/P01_BillsPaymentSystem/Data/BillsPaymentSystemContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_BillsPaymentSystem.Data;
+using P01_BillsPaymentSystem.Data.EntityConfig;
+using P01_BillsPaymentSystem.Data.Models;
+
+namespace P01_BillsPaymentSystem
+{
+    public class BillsPaymentSystemContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public BillsPaymentSystemContext()
+        {
+
+        }
+
+        public BillsPaymentSystemContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
