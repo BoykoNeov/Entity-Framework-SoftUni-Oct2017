b534395554e87bd631641fb0bfda2d177a4cd9ef
BoykoNeov <boikoneov@gmail.com>
2017-11-18 12:10:40 +0200

    tried to create the db, it is created, but relations seem mixed up2

--- AdvancedRelations/StartUp/StartUp.cs
+++ AdvancedRelations/StartUp/StartUp.cs
@@ -1,9 +1,16 @@
 ﻿namespace StartUp
 {
+    using P01_BillsPaymentSystem;
     public class StartUp
     {
         public static void Main()
         {
+            var db = new BillsPaymentSystemContext();
+
+            using (db)
+            {
+                db.Database.EnsureCreated();
+            }
         }
     }
 }
