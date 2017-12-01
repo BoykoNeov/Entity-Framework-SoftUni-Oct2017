94d7e310d90876fb2ea7aef53f7629108b4e70d4
BoykoNeov <boikoneov@gmail.com>
2017-12-01 18:27:40 +0200

    :hushed: some work on automapping exc...

---
+++ Automapping/Employees.Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employees.Data
+{
+   public class Configuration
+    {
+        public static string ConnectionString => $"Server=.;Database=EmployeesDb;Integrated Security=True";
+    }
+}
